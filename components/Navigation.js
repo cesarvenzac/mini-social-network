0adde646067b37df87e850c4f520f80a736c32fd
César Venzac <vzccesar@gmail.com>
2024-11-05 14:19:31 +0100

    feat: Added responsive to Navigation component

--- components/Navigation.js
+++ components/Navigation.js
@@ -11,7 +11,7 @@ class CustomNavigation extends HTMLElement {
                 <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
                 <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
               </svg>
-              Home
+              <span>Home</span>
             </a>
           </li>
           <li>
@@ -20,7 +20,7 @@ class CustomNavigation extends HTMLElement {
                 <rect width="20" height="16" x="2" y="4" rx="2" />
                 <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
               </svg>
-              Messages
+              <span>Messages</span>
             </a>
           </li>
           <li>
@@ -30,7 +30,7 @@ class CustomNavigation extends HTMLElement {
                 <circle cx="10" cy="8" r="5" />
                 <path d="M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3" />
               </svg>
-              Friends
+              <span>Friends</span>
             </a>
           </li>
         </ul>
